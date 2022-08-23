16d0aa51490618a5a21028c83e091abcf67f9f77
boumohamed <mb.bouzri@gmail.com>
2022-08-23 18:24:36 +0200

    init

---
+++ src/users/users.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+//import { UsersService } from './users.service';
+//import { UsersController } from './users.controller';
+import { MongooseModule } from "@nestjs/mongoose"
+import { UserSchema } from "./users.model"
+import { UsersService } from './users.service';
+import { UsersController } from './users.controller';
+
+@Module({
+    imports : [MongooseModule.forFeature([{ name: "user", schema: UserSchema }])],
+    providers: [UsersService],
+    controllers: [UsersController]
+})
+export class UsersModule {}
